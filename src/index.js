fffacee9297383c0d2be9d0cbe717e3500370f0e
macs <massimodemarchi91@gmail.com>
2021-02-03 00:15:33 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-export { default as ProductReviews } from './product_reviews/product_reviews.js';
-export { default as ReviewStars } from './review_stars/review_stars.js';
-export { default as ProductNote } from './product_note/product_note.js';
+// export { default as ProductReviews } from './product_reviews/product_reviews.js';
+// export { default as ReviewStars } from './review_stars/review_stars.js';
+// export { default as ProductNote } from './product_note/product_note.js';
 export { default as RecommendedProducts } from './recommended_products/recommended_products.js';
-export { default as NotifyProduct } from './notify_product/notify_product.js';
+// export { default as NotifyProduct } from './notify_product/notify_product.js';
 export { default as StoreDropdown } from './dropdowns/store_dropdown/store_dropdown.js';
-export { default as LanguageDropdown } from './dropdowns/language_dropdown/language_dropdown';
+// export { default as LanguageDropdown } from './dropdowns/language_dropdown/language_dropdown';
